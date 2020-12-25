9f13bc7baa38edd15a7fbefbbd7db1aa3eab78b5
kodlovis <418974104@qq.com>
2020-12-25 23:27:40 +0800

    重建中间表结构，将EvaluationKpi与User多对多关联，用于存放多个考评人

--- server/model/pas/evaluation_kpi.go
+++ server/model/pas/evaluation_kpi.go
@@ -1,15 +1,16 @@
 // 自动生成模板KpiTag
 package pas
-import "gin-vue-admin/model"
+import (
+  "gin-vue-admin/model"
+)
 
 // 如果含有time.Time 请自行import time包
 type EvaluationKpi struct {
-      Kpis []Kpi `json:"Kpis" gorm:"ForeignKey:ID;AssociationForeignKey:KpiId"`
-      KpiId uint `json:"KpiId" gorm:"primarykey"`
-      EvaluationId uint  `json:"EvaluationId"`
+      ID uint  `gorm:"primarykey"`
+      KpiId uint `json:"KpiId" gorm:"column:kpi_id;primarykey"`
+      EvaluationId uint  `json:"EvaluationId"  gorm:"column:evaluation_id;primarykey"`
       KpiScore  float64 `json:"KpiScore" form:"KpiScore" gorm:"column:KpiScore;comment:指标分数;type:float;"`
-      UserId   uint  `json:"UserId" gorm:"column:UserId;comment:用户ID;"`
-      Users []model.SysUser  `json:"Users" gorm:"ForeignKey:ID;references:UserId"`
+      Users []model.SysUser  `gorm:"many2many:evaluationKpi_user;foreignKey:ID;References:ID"`
 }
 
 
