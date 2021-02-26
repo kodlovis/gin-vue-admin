8346a05e8f3093e255763240fd0f27c65eab1701
kodlovis <418974104@qq.com>
2021-02-26 15:39:24 +0800

    修改默认头像

--- server/model/sys_user.go
+++ server/model/sys_user.go
@@ -11,7 +11,7 @@ type SysUser struct {
 	Username    string       `json:"userName" gorm:"comment:用户登录名"`
 	Password    string       `json:"-"  gorm:"comment:用户登录密码"`
 	NickName    string       `json:"nickName" gorm:"default:系统用户;comment:用户昵称" `
-	HeaderImg   string       `json:"headerImg" gorm:"default:http://qmplusimg.henrongyi.top/head.png;comment:用户头像"`
+	HeaderImg   string       `json:"headerImg" gorm:"default:uploads/file/2382c315a6ba396be1932dd8dafaff52_20210225153524.png:用户头像"`
 	Authority   SysAuthority `json:"authority" gorm:"foreignKey:AuthorityId;references:AuthorityId;comment:用户角色"`
 	AuthorityId string       `json:"authorityId" gorm:"default:888;comment:用户角色ID"`
 }
