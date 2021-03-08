645bb8a0324c0dc864db63f368281208d9c5977c
kodlovis <418974104@qq.com>
2021-03-08 15:06:41 +0800

    修改批量驳回的算法

--- server/service/pas/performance_review_item_user.go
+++ server/service/pas/performance_review_item_user.go
@@ -81,7 +81,7 @@ func GetPRIUListByUser(id uint, status uint, info rp.PerformanceReviewItemUserSe
 	// 如果有条件搜索 下方会自动创建搜索语句
 	err = db.Count(&total).Error
 	err = db.Limit(limit).Offset(offset).Find(&PerformanceReviewItemUsers).Error
-	err = db.Preload("PRI.Kpi").Preload("PRI.PRs.User").Preload("User").Find(&PerformanceReviewItemUsers).Error
+	err = db.Preload("PRI.Kpi.Tags").Preload("PRI.PRs.User").Preload("User").Find(&PerformanceReviewItemUsers).Error
 	return err, PerformanceReviewItemUsers, total
 }
 
@@ -128,28 +128,29 @@ func UpdatePRIUStatusByIds(ids []uint,status uint) (err error) {
 	var num = int64(0)
 	var PRIUs []mp.PerformanceReviewItemUser
 	err = global.GVA_DB.Where("id in ?", ids).Find(&PRIUs).Error
+	var arr []uint
+	//计算priu有没有都驳回
 	for i := 0; i < len(PRIUs); i++ {
 		err = global.GVA_DB.Model(&PRIU).Where("pri_id = ? AND status = ?", PRIUs[i].PRIID, status).Count(&num).Error
 		err = global.GVA_DB.Model(&PRIU).Where("pri_id = ?",PRIUs[i].PRIID).Count(&total).Error
 		var count = float64(num) / float64(total)
+		//如果全都驳回 count=1，进入运算
 		if count ==1  {
-			var arr []uint
-			for i := 0; i < len(PRIUs); i++ {
 				arr = append(arr,PRIUs[i].PRIID)
-			}
+		}
+	}
+			//更新PRI状态
 			err = global.GVA_DB.Model(&PRI).Where("id in ?", arr).Select("status").Updates(map[string]interface{}{"status": status}).Error
-			err = global.GVA_DB.Where("id = ?", PRIUs[i].PRIID).Find(&PRI).Error
+			err = global.GVA_DB.Where("id = ?", PRIUs[0].PRIID).Find(&PRI).Error
 			var prTotal = int64(0)
 			var prNum = int64(0)
-			err = global.GVA_DB.Model(&PRI).Where("id = ? AND status = ?", PRIUs[i].PRIID, status).Count(&prNum).Error
-			err = global.GVA_DB.Model(&PRI).Where("id = ?",PRIUs[i].PRIID).Count(&prTotal).Error
+			err = global.GVA_DB.Model(&PRI).Where("id = ? AND status = ?", PRIUs[0].PRIID, status).Count(&prNum).Error
+			err = global.GVA_DB.Model(&PRI).Where("id = ?",PRIUs[0].PRIID).Count(&prTotal).Error
 			var prCount = float64(prNum) / float64(prTotal)
 			var PR mp.PerformanceReview
 			err = global.GVA_DB.Where("id = ?", PRI.PRId).Find(&PR).Error
 			if prCount ==1&&PR.Status==8 {
 				err = global.GVA_DB.Model(&PR).Where("id = ?", PR.ID).Select("status").Updates(map[string]interface{}{"status": 9}).Error
 			}
-		}
-	}
 	return err
 }
