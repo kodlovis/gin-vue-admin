b743c07a42eafc642fe273997c128e1fc5e4ca79
kodlovis <418974104@qq.com>
2021-04-25 16:57:38 +0800

    修改宽度

--- web/src/view/pas/myAssessment/result.vue
+++ web/src/view/pas/myAssessment/result.vue
@@ -82,8 +82,8 @@
         </el-table-column>
         <el-table-column label="指标名称" prop="kpi.name" width="120"></el-table-column> 
         
-        <el-table-column label="指标算法" prop="kpi.category" width="460"></el-table-column> 
-        <el-table-column label="指标描述" prop="kpi.description" width="460"></el-table-column> 
+        <el-table-column label="指标算法" prop="kpi.category" width="420"></el-table-column> 
+        <el-table-column label="指标描述" prop="kpi.description" width="420"></el-table-column> 
         <el-table-column label="指标分值" prop="score" width="100"></el-table-column>
         <el-table-column label="指标得分" prop="result" width="100"></el-table-column>
         <el-table-column label="评分人及评分" prop="PRIUs.user.nickName" width="160">
@@ -92,7 +92,7 @@
             :key="index">{{item.user.nickName}}:{{item.result}}分<br/></span>
           </template>
         </el-table-column>
-        <el-table-column label="评分人备注" prop="comment" width="100"></el-table-column>
+        <el-table-column label="评分人备注" prop="comment"></el-table-column>
         </el-table>
     
 
