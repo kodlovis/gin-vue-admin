9d264b8ddaadc6b4c9ca2917d6ebfde7bef9e708
kodlovis <418974104@qq.com>
2021-04-28 09:19:30 +0800

    修改最小限制

--- web/src/view/superAdmin/user/user.vue
+++ web/src/view/superAdmin/user/user.vue
@@ -123,11 +123,11 @@ export default {
       rules: {
         username: [
           { required: true, message: "请输入用户名", trigger: "blur" },
-          { min: 6, message: "最低6位字符", trigger: "blur" }
+          { min: 2, message: "最低2位字符", trigger: "blur" }
         ],
         password: [
           { required: true, message: "请输入用户密码", trigger: "blur" },
-          { min: 6, message: "最低6位字符", trigger: "blur" }
+          { min: 2, message: "最低2位字符", trigger: "blur" }
         ],
         nickName: [
           { required: true, message: "请输入用户昵称", trigger: "blur" }
